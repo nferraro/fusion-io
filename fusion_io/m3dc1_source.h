e6a1857f18d2f6246f6ab4ab72892b9f9c4f1523
andreas-kleiner <52830013+andreas-kleiner@users.noreply.github.com>
2020-02-10 14:17:22 -0500

    Added parameter for toroidal mode number

--- fusion_io/m3dc1_source.h
+++ fusion_io/m3dc1_source.h
@@ -10,7 +10,7 @@ class m3dc1_source : public fio_source {
   m3dc1_file file;
   double bzero, rzero, z_ion, ion_mass, period;
   double n0, L0, B0, p0, t0, v0, J0, Phi0, temp0;
-  int linear, eqsubtract, extsubtract, icomplex, i3d, version, itor, ntime;
+  int linear, eqsubtract, extsubtract, icomplex, i3d, version, itor, ntor, ntime;
   fio_species ion_species;
 
  public:
