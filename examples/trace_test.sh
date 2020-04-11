03e9b3938df680c5837d9e6f14467e61257917f8
Nathaniel Ferraro <nathaniel.ferraro@gmail.com>
2020-04-11 17:21:05 -0400

    Added geqdsk option to trace_test.sh

--- examples/trace_test.sh
+++ examples/trace_test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-time $FIO_ROOT/trace/_$FIO_ARCH/trace -m3dc1 data/m3dc1/C1.h5 -1 -dR 0.1 -p 8 -t 1000 -s 300
+if [[ "$1" == "m3dc1" ]]; then
+    FILE="-m3dc1 data/m3dc1/C1.h5 -1 -m3dc1 data/m3dc1/C1.h5 1 5."
+elif [[ "$1" == "geqdsk" ]]; then
+    FILE="-geqdsk data/geqdsk/g158115.04701"
+fi
+
+if [ -z "$FILE" ]; then
+    echo "Please choose one of:"
+    echo " m3dc1"
+    echo " geqdsk"    
+else
+    time $FIO_ROOT/trace/_$FIO_ARCH/trace $FILE -dR 0.05 -p 12 -t 1000 -s 300
+fi
