34557ff447ffa4698d79ce97a3229b3aa3f4caca
Nate Ferraro <nathaniel.ferraro@gmail.com>
2020-07-16 14:59:51 -0400

    Tweaks to trace_test parameters

--- examples/trace_test.sh
+++ examples/trace_test.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "m3dc1" ]]; then
 elif [[ "$1" == "geqdsk" ]]; then
     FILE="-geqdsk data/geqdsk/g158115.04701"
 elif [[ "$1" == "gpec" ]]; then
-    FILE="-gpec data/gpec 0 0.2"
+    FILE="-gpec data/gpec 0 1"
 fi
 
 if [ -z "$FILE" ]; then
@@ -14,5 +14,5 @@ if [ -z "$FILE" ]; then
     echo " geqdsk"    
     echo " gpec"
 else
-    time $FIO_ROOT/trace/_$FIO_ARCH/trace $FILE -dR 0.05 -p 12 -t 1000 -s 300
+    time $FIO_ROOT/trace/_$FIO_ARCH/trace $FILE -dR 0.025 -p 16 -t 1000 -s 300
 fi
