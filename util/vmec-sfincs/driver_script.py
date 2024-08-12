0ff685d04fe4402ab10f0436a53eb70f5ac9428a
Michael Martin <mm1889@stellar-intel.princeton.edu>
2024-08-12 16:53:05 -0400

    Added Aminor_p to imitation VMEC; Fixed finite difference errors in Jacobian calc; minor changes

--- util/vmec-sfincs/driver_script.py
+++ util/vmec-sfincs/driver_script.py
@@ -7,15 +7,16 @@ import m3dc1_to_vmec_sfincs as convert
 
 sys.path.append(os.environ['FIO_ROOT']+"util/sfincs")
 
-m3dc1_outfile = './C1.h5'
-te_start = 16000 # eV
-te_end = 9000 # eV
+m3dc1_outfile = '../C1.h5'
+te_start = 1200 # eV
+te_end = 100 # eV
 timeslice = -1
 ntheta = 400
-nphi = 80
-nsurf = 5 # Value is doubled in write_neo_input to account for the half mesh needed by VMEC
-nfp = 2
+nphi = 32
+nsurf = 50 # Value is doubled in write_neo_input to account for the half mesh needed by VMEC
+nfp = 1
 ntor = 6
 mpol = 8
+dR0 = 0.83
 
-convert.m3dc1_to_vmec(m3dc1_outfile, timeslice, nfp, ntheta, nphi, nsurf, vmec_mpol=mpol, vmec_ntor=ntor, te_start=te_start, te_end=te_end, dR0=3.24, vmec_file_descriptor="test", write_sfincs_input=True)
+convert.m3dc1_to_vmec(m3dc1_outfile, timeslice, nfp, ntheta, nphi, nsurf, vmec_mpol=mpol, vmec_ntor=ntor, te_start=te_start, te_end=te_end, dR0=dR0, vmec_file_descriptor="new_tcv", write_sfincs_input=True)
